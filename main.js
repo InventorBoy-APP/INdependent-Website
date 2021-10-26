835b04c21ccbb58d4ef9a6f9bbd88b32cb851cce
InventorBoy-APP <78768396+InventorBoy-APP@users.noreply.github.com>
2021-10-26 19:23:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function bathroomtaker(){
+    window.location="bathroom.html"
+}
+
+
+function kitchentaker(){
+    window.location="kitchen.html"
+}
+
+
+function bedroomtaker(){
+    window.location="bedroom.html"
+}
+
+
+function livingroomtaker(){
+    window.location="livingroom.html"
+}
